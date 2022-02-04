bc269affcd800380464f0542b9b9fbcdb299b5a7
Iara <iaraflorencia2@gmail.com>
2022-02-04 14:50:20 -0300

    Ejercicio nueve bis e/s

--- 1-EntradaSalida/jsEntradaSalida-09.js
+++ 1-EntradaSalida/jsEntradaSalida-09.js
@@ -2,16 +2,20 @@
 Debemos lograr tomar el importe por ID ,
 transformarlo a entero (parseInt), luego
 mostrar el importe con un aumento del 10 %
-en el cuadro de texto "RESULTADO".*/
+en el cuadro de texto "RESULTADO".
+9 bis 
+se debe pedir el % por prompt. */
 function mostrarAumento()
 {
 	let sueldo;
 	let aumento;
 	let txtIdresultado;
+	let descuento;
 
 	sueldo = document.getElementById("txtIdSueldo").value;
 	sueldo = parseInt(sueldo);
-	aumento = sueldo*0.10;
+	descuento = prompt("Ingrese el aumento")
+	aumento = sueldo*descuento/100;
 	aumento = parseInt(aumento);
 
 	txtIdresultado = aumento+sueldo;
