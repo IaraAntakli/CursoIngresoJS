10a36041f3bf3585277f05f72b15ea7941d99367
Iara <iaraflorencia2@gmail.com>
2022-02-04 15:04:35 -0300

    Ejercicio 10 bis e/s

--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -2,19 +2,30 @@
 Debemos lograr tomar el importe por ID.
 Transformarlo a entero (parseInt), luego
 mostrar el importe con un Descuento del 25 %
-en el cuadro de texto "RESULTADO"*/
+en el cuadro de texto "RESULTADO"
+-
+se debe pedir el % por prompt
+cambiamos la salida:
+Mostrar por alert el importe, la cantidad del descuento, el importe con el descuento
+ej: "El importe es 1000 la cantidad de descuento es 250 y el importe con el descuento es 750"*/
 function mostrarAumento()
 {
 	let importe;
 	let descuento;
+	let porcentaje;
 	let txtIdresultado;
 
 	importe = document.getElementById("txtIdImporte").value;
 	importe = parseInt(importe);
-	descuento = importe*0.25;
+
+	porcentaje = prompt("Ingrese el descuento");
+	porcentaje = parseInt(porcentaje);
+
 	descuento = parseInt(descuento);
+	descuento = importe*porcentaje/100;	
 
 	txtIdresultado = importe-descuento;
 
-	alert(txtIdresultado);
+	alert("El importe es "+importe+", la cantidad de descuento es de "+descuento+ " y el importe con descuento es "+txtIdresultado);
+
 }
