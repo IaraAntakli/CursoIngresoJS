4b35911ef4d4d3f3a4f03f7a63ffcefefaa68cf7
Iara <iaraflorencia2@gmail.com>
2022-02-09 15:40:51 -0300

    Tercer ejercicio If

--- 2-InstruccionIf/jsInstruccionIF-03.js
+++ 2-InstruccionIf/jsInstruccionIF-03.js
@@ -1,6 +1,17 @@
 function mostrar()
 {
-	//tomo la edad  
-	alert("ok");
+	/* Al ingresar una edad debemos informar si la persona es mayor de edad, 
+	sino informar que es un menor de edad*/
+
+	var edadIngresada;
+
+	edadIngresada = document.getElementById("txtIdEdad").value;
+	edadIngresada = parseInt(edadIngresada);
+
+	if(edadIngresada >= 18)
+	{
+		alert("Usted es mayor de edad");
+	}
+	else(alert("Usted es menor de edad"));
 
 }//FIN DE LA FUNCIÓN
