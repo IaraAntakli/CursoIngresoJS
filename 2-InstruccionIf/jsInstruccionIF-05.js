dd673e7b3aae305f26a4ef4d1fe659649097cab0
Iara <iaraflorencia2@gmail.com>
2022-02-09 16:54:08 -0300

    Quinto ejercicio If

--- 2-InstruccionIf/jsInstruccionIF-05.js
+++ 2-InstruccionIf/jsInstruccionIF-05.js
@@ -1,6 +1,23 @@
 function mostrar()
 {
-	//tomo la edad  
-	alert("ok");
+	/*Al ingresar una edad solo debemos informar si la persona NO es adolescente.*/
 
+	var edadIngresada;
+
+	edadIngresada = document.getElementById("txtIdEdad").value;
+	edadIngresada = parseInt(edadIngresada);
+
+	if(edadIngresada >= 18)
+	{
+		alert("Usted no es un adolescente");
+	}
+ 	if(edadIngresada <= 12)
+ 	{
+ 		alert("Usted no es un adolescente");
+ 	}
+
+ 	
+	//&& edadIngresada <= 12
+
+	
 }//FIN DE LA FUNCIÓN
