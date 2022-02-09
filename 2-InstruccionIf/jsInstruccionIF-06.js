94824057c8612d6bd3968695495678de775b9abd
Iara <iaraflorencia2@gmail.com>
2022-02-09 17:00:32 -0300

    Ejercicio seis If

--- 2-InstruccionIf/jsInstruccionIF-06.js
+++ 2-InstruccionIf/jsInstruccionIF-06.js
@@ -1,8 +1,25 @@
 function mostrar()
 {
-	//tomo la edad  
-	alert("ok");
+	/*Al ingresar una edad debemos informar si la persona es mayor de edad (mas de 18 años)
+	o adolescente (entre 13 y 17 años) o niño (menor a 13 años).*/
 
+	var edadIngresada;
 
+	edadIngresada = document.getElementById("txtIdEdad").value;
+	edadIngresada = parseInt(edadIngresada);
+
+	if(edadIngresada >= 13 && edadIngresada <= 17)
+	{
+		alert("Usted es un adolescente");
+	}
+	if(edadIngresada >= 18)
+	{
+		alert("Usted es mayor de edad");
+	}
+ 	if(edadIngresada <= 12)
+ 	{
+ 		alert("Usted es un niño");
+ 	}  
+	
 
 }//FIN DE LA FUNCIÓN
