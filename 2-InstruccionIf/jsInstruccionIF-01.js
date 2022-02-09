05d006356571a77437cf15d0cab119b20f403623
Iara <iaraflorencia2@gmail.com>
2022-02-09 15:01:54 -0300

    Primer Ejercicio If

--- 2-InstruccionIf/jsInstruccionIF-01.js
+++ 2-InstruccionIf/jsInstruccionIF-01.js
@@ -1,6 +1,14 @@
 function mostrar()
 {
 	//tomo la edad  
-	alert("ok");
+
+	var edadIngresada;
+
+	edadIngresada = document.getElementById("txtIdEdad").value;
+	edadIngresada = parseInt(edadIngresada);
+
+	if(edadIngresada == 15)
+	alert("Niña bonita");
+	else(alert("Edad no adecuada"));
 
 }//FIN DE LA FUNCIÓN
